98cebc3f77d49243d9f0fabb5a32e827257beef5
interactive-directory-1839 <benthedunn@gmail.com>
2020-06-03 18:53:09 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  original = string.split(" ")
+  new = []
+  original.each do |string|
+    new << string.reverse
+  end
+new.join(" ")
+end
+
+def reverse_each_word(string)
+  original = string.split(" ")
+  new = []
+  original.collect do |string|
+    new << string.reverse
+  end
+  new.join(" ")
+end
